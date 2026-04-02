5fb6ea9e77e778e8fdcb1ab2f7f3eed725517d43
Студент Студентов <is@athene.tech>
2026-04-02 09:40:56 +0300

    final

--- Program.cs
+++ Program.cs
@@ -1,15 +1,27 @@
 ﻿using OOP111;
-using System.Collections.Generic;
+using System;
 
-List<Person> people = new List<Person>();
-
-people.Add(new Student("Иванов Иван", 2003, 101, "А-21", 4.5));
-people.Add(new Student("Петрова Мария", 2004, 102, "Б-22", 4.8));
-people.Add(new Prepod("Смирнов Алексей", 1980, 201, "Математика", "Высшая"));
-people.Add(new Prepod("Кузнецова Елена", 1975, 202, "Физика", "Кандидат наук"));
-
-foreach (Person p in people)
+class Program
 {
-	p.Study();
-	p.Print();
+	static void Main()
+	{
+		var department = new UniversityDepartment<Person>();
+
+		department.AddMember(new Student("Иванов Иван", 2003, 101, "А-21", 4.5));
+		department.AddMember(new Student("Петрова Мария", 2004, 102, "Б-22", 4.8));
+		department.AddMember(new Prepod("Смирнов Алексей", 1980, 201, "Математика", "Высшая"));
+		department.AddMember(new Prepod("Кузнецова Елена", 1975, 202, "Физика", "Кандидат наук"));
+
+		Console.WriteLine("Все члены кафедры:");
+		department.PrintAll();
+
+		Console.WriteLine("\nПоиск по имени 'Иван':");
+		var found = department.FindByName("Иван");
+		foreach (var p in found)
+		{
+			p.Print();
+		}
+
+		Console.WriteLine($"\nОбщее количество членов кафедры: {UniversityDepartment<Person>.TotalMembersCount}");
+	}
 }
---
+++ UniversityDepartment.cs
@@ -0,0 +1,34 @@
+﻿using OOP111;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OOP111
+{
+	public class UniversityDepartment<T> where T : Person
+	{
+		private List<T> _members = new List<T>();
+		private static int _totalMembersCount = 0;
+
+		public static int TotalMembersCount => _totalMembersCount;
+
+		public void AddMember(T person)
+		{
+			_members.Add(person);
+			_totalMembersCount++;
+		}
+
+		public List<T> FindByName(string name)
+		{
+			return _members.Where(p => p.fullName.IndexOf(name, StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+		}
+
+		public void PrintAll()
+		{
+			foreach (var person in _members)
+			{
+				person.Print();
+			}
+		}
+	}
+}
