758f1c8cb8ff978174526a8f3b74ab8b97baa3fb
AdmiN <maxim75099@gmail.com>
2026-04-29 22:32:46 +0400

    2

--- DrawningTank.cs
+++ DrawningTank.cs
@@ -133,18 +133,14 @@ public class DrawingTank
 
 
 
-        // БАШНЯ (смещена левее)
-        int turretX = bodyX + bodyW / 2 - 50;
+        // БАШНЯ по центру корпуса
+        int turretX = bodyX + bodyW / 2 - 30;   // смещение влево от центра
         int turretY = y + 8;
         int turretW = 44;
         int turretH = 13;
 
         g.FillRectangle(new SolidBrush(armorColor), turretX, turretY, turretW, turretH);
         g.DrawRectangle(outlinePen, turretX, turretY, turretW, turretH);
-        g.FillEllipse(new SolidBrush(armorColor), turretX + 5, turretY - 4, turretW - 10, 9);
-        g.DrawEllipse(outlinePen, turretX + 5, turretY - 4, turretW - 10, 9);
-        g.FillRectangle(new SolidBrush(armorColor), turretX + 25, turretY - 3, 10, 5);
-        g.DrawRectangle(outlinePen, turretX + 25, turretY - 3, 10, 5);
 
     }
 }
